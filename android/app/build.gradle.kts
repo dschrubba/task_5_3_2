2f9a0f2d36aa78882c561593462eb81ee1ed56a9
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-08-25 13:00:56 +0200

    Aufgabe Gelöst

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.task_5_3_2"
+    namespace  = "com.example.task_5_3_2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MainApp(
+    sharedPrefs: await SharedPreferences.getInstance()
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+
+  static const String _nameKey = "name";
+  static const String _modeKey = "mode";
+  final SharedPreferences sharedPrefs;
+  const MainApp({super.key, required this.sharedPrefs});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+
+    final TextEditingController controller = TextEditingController();
+
+    bool   switchValue = widget.sharedPrefs.getBool(MainApp._modeKey)   ?? false;
+    String displayName = widget.sharedPrefs.getString(MainApp._nameKey) ?? " ";
+
+    void saveName(String name) {
+      widget.sharedPrefs.setString(MainApp._nameKey, name);
+      setState(() {
+        controller.clear();
+        displayName = name;
+      });
+    }
+
+    void reset(TextEditingController controller) {
+      setState(() {
+        controller.clear();
+        widget.sharedPrefs.remove(MainApp._nameKey); 
+      });
+    }
+
+    void onSwitchToggle(bool value) {
+      widget.sharedPrefs.setBool(MainApp._modeKey, value);
+      setState(() {
+        
+      });
+    }
+
+    ThemeData getTheme(bool darkMode) {
+      if (!darkMode) {
+        return ThemeData.light(useMaterial3: true);
+      } else {
+        return ThemeData.dark(useMaterial3: true);
+      }
+    }
+
+    return MaterialApp(
+      theme: getTheme(switchValue),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          actions: [
+            Switch(value: switchValue, onChanged: (bool value) => onSwitchToggle(value))
+          ],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Flex(
+                spacing: 32,
+                mainAxisSize: MainAxisSize.min,
+                direction: Axis.vertical,
+                children: [
+                  TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hint: Text("Enter your Name")
+                    ),
+                  ),
+                  Text(displayName.trim().isNotEmpty ? "I remember you. Your name is $displayName" : " "),
+                  ElevatedButton(onPressed: () => {saveName(controller.text)}, child: Text("Save your name")),
+                  ElevatedButton(onPressed: () => {reset(controller)}, child: Text("Reset")),                 
+                ],
+              ),
+            )
+          ),
         ),
       ),
     );
